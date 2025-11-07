94e44e44ace4a87a31626be7c30fd033e63173b3
nashimoff <nashimoff@gmail.com>
2025-11-07 17:11:19 +0600

    a

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,5 +1,7 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,9 +33,48 @@ public class ProducerDemo {
         properties.setProperty("value.serializer",StringSerializer.class.getName());
 
         // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java","hello world");
 
         // send data
+        producer.send(producerRecord);
+
+        // tell the producer to send all data and block until done --synchronous
+        producer.flush();
 
         // flush and close the producer
+        producer.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
