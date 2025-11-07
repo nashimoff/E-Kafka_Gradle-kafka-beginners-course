40ed764228e8b0342ad279f7bc839b0757549361
nashimoff <nashimoff@gmail.com>
2025-11-07 16:11:03 +0600

    sdfs

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -15,8 +15,15 @@ public class ProducerDemo {
 
         // create Producer Properties
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
 
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // connect to Conduktor Playground
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // create the Producer
 
