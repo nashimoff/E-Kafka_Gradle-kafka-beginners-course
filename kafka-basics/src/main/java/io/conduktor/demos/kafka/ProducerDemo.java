c7dd14b013071e21f311dd866ced306a14e8064d
nashimoff <nashimoff@gmail.com>
2025-11-10 10:39:39 +0600

    a

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -14,7 +14,7 @@ public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
     public static void main(String[] args) {
 
-       log.info("Hello world!");
+       log.info("I am a Kafka Producer!");
 
         // create Producer Properties
         Properties properties = new Properties();
